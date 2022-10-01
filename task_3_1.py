011b6532a1f04119461f967087770504dcd8d220
Dmirty <9048881951@mail.ru>
2022-10-01 22:38:35 +0500

    01.10

--- task_3_1.py
+++ task_3_1.py
@@ -13,3 +13,12 @@
 # out
 # [4, 2, 4, 9]
 # 8
+
+from random import sample, randint
+def create_new_list(num):
+    if num < 0:
+        return "Negative value of the number of numbers!"
+    list_num = sample(range(1, num * 2), num)
+    print(list_num)
+    return list_num
+print(sum(create_new_list(int(input('Количество чисел:')))[::2]))
