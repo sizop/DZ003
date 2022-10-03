67080fa07d33c13914be161161282f46d945f050
Dmirty <9048881951@mail.ru>
2022-10-03 11:51:03 +0500

    +3.5

--- task_3_1.py
+++ task_3_1.py
@@ -13,14 +13,9 @@
 # out
 # [4, 2, 4, 9]
 # 8
+
 from function import cr_nln
 
-# def create_new_list(num):
-#     if num < 0:
-#         return "Negative value of the number of numbers!"
-#     list_num = sample(range(1, num * 2), num)
-#     print(list_num)
-#     return list_num
 my_list = cr_nln(int(input('Количество чисел:')))
 print(my_list)
 print(sum(my_list[::2]))
