60ed38de69cda4627f684fabf31da6b1698dbb19
Dmirty <9048881951@mail.ru>
2022-10-04 10:20:07 +0500

    +4.3

--- function.py
+++ function.py
@@ -1,6 +1,8 @@
 from random import sample
-def cr_nln(num):
-        if num > 0:
-            list_num = sample(range(1, num * 2),num)
-            return list_num
-        return "Negative value of the number of numbers!"
+
+
+def cr_nln():
+    if num > 0:
+        list_num = sample(range(1, num * 2), num)
+        return list_num
+    return "Negative value of the number of numbers!"
